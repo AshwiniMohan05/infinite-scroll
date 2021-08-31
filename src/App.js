5464fdad23f37aea148cfdca433d61eac1afd605
Ashwini Mohan <ashwinimohan@Ashwinis-MacBook-Pro.local>
2021-08-31 17:10:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, PureComponent} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      texts: [],
+      loading: false,
+      pageNo: 0,
+      query: ""
+    }
+  } 
+
+  componentDidMount(){
+    const { pageNo } = this.state;
+    this.getRandomText(pageNo)
+    let observer;
+    let options = {
+      root:null,
+      rootMargin: '0px',
+
+    }
+    // this.observer = new IntersectionObserver(this.handlePageObserver.bind(this), options)
+    
+    
+    observer = new IntersectionObserver(this.handlePageObserver.bind(this), options);
+    observer.observe(this.loadingRef);
+  }
+
+  handlePageObserver(entity){
+    console.log('this.state.texts', this.state.texts);
+    const { previousPage, texts} = this.state;
+    const y = entity[0].boundingClientRect.y;
+    console.log('y', y);
+    if(previousPage > y){
+      console.log('hey', texts)
+      const lastText = texts.length - 1;
+      this.getRandomText(lastText)
+    }
+    this.setState({ previousPage: y })
+  }
+
+  getRandomText = (pageNo) => {
+    this.setState({ loading: true });
+    const apiUrl = `http://openlibrary.org/search.json?q=the+lord+of+the+rings&${pageNo}&limit=5`;
+    fetch(apiUrl).then(response => response.json())
+    .then(data => {
+      this.setState({ texts: data })
+      this.setState({  loading: false})
+      console.log('data', data);
+    })
+  }
+
+
+  render(){
+    const { texts, query, loading } = this.state;
+    const textObject  = texts && texts.docs;
+    console.log('textObject', textObject);
+    const loadingCss = {
+      height: "100px",
+      margin: "30px"
+    }
+    console.log('loading', loading)
+    return(
+      <>
+      {
+          loading ? <h1>Loading...</h1>: 
+          <div>
+        <input type="text" value={query} onChange={this.handleChange} />
+
+       {
+         textObject && textObject.map(item => {
+           console.log('item', item)
+           return(
+             <div>
+               {item.text.map(displayText => {
+                 return(
+                   <h5>{displayText}</h5>
+                 )
+               })}
+              </div> 
+           )
+         })
+       }
+       <div ref={loadingRef => (this.loadingRef = loadingRef)} style={loadingCss}>
+
+       </div>
+      </div>
+      }
+      </>
+      
+    )
+  }
 }
 
 export default App;
